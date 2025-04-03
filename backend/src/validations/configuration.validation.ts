10b695c7e69b9945da192967dbcd81db6ef6f42a
Dima <dimaklim04@gmail.com>
2025-04-03 21:15:14 +0300

    edit validation

--- backend/src/validations/configuration.validation.ts
+++ backend/src/validations/configuration.validation.ts
@@ -1,10 +1,22 @@
 import { body } from "express-validator";
 import { sectionTitles, sectionTypes } from "../models/configuration.model";
+import { Types } from "mongoose";
 
 export const configurationValidation = [
   body("name").isString().notEmpty(),
   body("isMain", "Is main must be a boolean").isBoolean(),
-  body("sections", "Sections is required").isArray({ min: 5 }),
+  body("sections", "Sections is required")
+    .isArray({ min: 1 })
+    .custom((sections) => {
+      for (const section of sections) {
+        const ids = section.items.map((id: Types.ObjectId) => id.toString());
+        const uniqueIds = new Set(ids);
+        if (uniqueIds.size !== ids.length) {
+          throw new Error("Duplicate items found in a section");
+        }
+      }
+      return true;
+    }),
   body("sections.*.type", "Section type is invalid").isIn(sectionTypes),
   body("sections.*.showTitle", "Show title must be a boolean").isBoolean(),
   body(
